16cce87b56d5eaccf22dc0975629e2c0615d9ca0
Oscar Fuentes <ofuentes@FSUMMA.LOCAL>
2022-03-16 16:30:52 -0600

    Build: First table created

--- data.js
+++ data.js
@@ -1,15 +1,16 @@
 var assetType = [
     {
-        "name":"Inmuebles",
+        "name":"Macbooks",
         "di": 0.33,
-        "months": 12,
+        "months": 36,
         "dc": 0.55,
         "dm": 0.85,
         "vue": 5,
+        "da": 0.17,
+        "fp": 0.0152790649144242,
     },
     {
         "name":"Maquinaria amarilla"
-
     },
     {
         "name":"Vehículos"
--- main.js
+++ main.js
@@ -24,6 +24,38 @@ window.onload = function() {
             exchange.style.display = "none"; 
         }
     });
-};
+    
+    
+    let calc = document.getElementById("calculate");
+    
+    calc.addEventListener('click',()=>{
+        
+        document.getElementById('spreadsheet').innerHTML = '';
 
+        var dc = 1 - assetType[0].dc,
+        di = 1 - assetType[0].di,
+        dm = 1 - assetType[0].dm,
+        m = assetType[0].months,
+        f = Math.log((di - dm)/(dc - dm))/m;
+        val = document.getElementById('value').value,
+        p = document.getElementById('months').value;
+        
+        var data = [];
+
+        for (let i = 0; i <= p; i++){
+            let v = val * ((di - dm) * Math.exp(-i*f) + dm);
+            data.push([ i, (di - dm) * Math.exp(-i*f) + dm, v]);
+        }
+
+        
+        jspreadsheet(document.getElementById('spreadsheet'), {
+            data:data,
+            columns: [
+                { type: 'text', title: 'Mes', width: 60 },
+                { type: 'numeric', title:'Depreciación', width:120, mask:'0%' },
+                { type: 'numeric', title:'Valor', width:150, mask:'Q #,##0.00'},
+            ]
+        });
+    });
+};
 
